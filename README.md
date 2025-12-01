2bc11300f5cb47603f1caed0b3a88b4e4b72501b
Adrian Calavie <adrian.calavie06@gmail.com>
2025-12-01 21:28:03 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+
+![Project Icon](./res/pack.png)
+# Xaero Icons for Untitled Goose Mod
+
+
+
+Adds icons for Untitled Duck Mod for Xaero's Minimap
---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from zipfile import ZIP_DEFLATED, ZipFile
+
+RESOURCEPACK_FILES_PATH = "./res"
+OUT_PATH = "./out"
+RESOURCEPACK_NAME = "XaeroIconsUntitledGoose"
+
+
+def zip_resourcepack():
+
+    os.makedirs(OUT_PATH, exist_ok=True)
+    with ZipFile(os.path.join(OUT_PATH, f"{RESOURCEPACK_NAME}.zip"), "w", compression=ZIP_DEFLATED) as zip_file:
+
+        for root, _, files in os.walk(RESOURCEPACK_FILES_PATH):
+            for file in files:
+                zip_file.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), RESOURCEPACK_FILES_PATH))
+
+if __name__ == "__main__":
+    zip_resourcepack()
